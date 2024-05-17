5ccda99a7bc07236418abce97b51248d2e942cc8
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-05-18 01:27:08 +0400

    2 of each routes done starting front

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,30 @@
 import pool from "../lib/db.js";
 
-export const userUpdate = async(req,res) =>{
-    const {id} = req.params
-    const {profile_picture,bio} = req.body
-    try{
-       const [updatedProfile] =await pool.query(
-        `UPDATE users SET bio=?,profile_picture=?WHERE id =?`,[bio,profile_picture,id]
-    )   
-    if(updatedProfile.affectedRows===0){
-        return res.status(404).json({msg:"User not found"})
+export const userUpdate = async (req, res) => {
+  const { id } = req.params;
+  const { profile_picture, bio } = req.body;
+  try {
+    const [updatedProfile] = await pool.query(
+      `UPDATE users SET bio=?,profile_picture=? WHERE id = ?`,
+      [bio, profile_picture, id]
+    );
+    if (updatedProfile.affectedRows === 0) {
+      return res.status(404).json({ msg: "User not found" });
     }
-    return res.status(200).json({msg:"User updated succesfully"})
-    }catch(Err){
-        res.status(500).json({msg:"Something went wrong"},Err)
+    return res.status(200).json({ msg: "User updated succesfully" });
+  } catch (Err) {
+    console.log(Err);
+  }
+};
+export const getUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [user] = await pool.query(`SELECT * FROM users WHERE id=?`, [id]);
+    if (user.length === 0) {
+      return res.status(404).json({ msg: "User not found" });
     }
-  
-}
-// export const getUser = async(req,res)=>{
-    
-// }
+    return res.status(200).json(user[0]);
+  } catch (Err) {
+    console.log(Err);
+  }
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import { userUpdate } from '../controllers/userController.js'
+import { getUser, userUpdate } from '../controllers/userController.js'
 
 const router = express.Router()
 router.put('/update/:id',userUpdate)
+router.get('/get/:id',getUser)
 export default router
